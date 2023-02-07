115b0b3e6e0890251f6a3fdb85d58a3ebfba777e
sjkng <zburleastefan@yahoo.com>
2023-02-07 21:32:46 +0200

    First commit

---
+++ WebApp1/Controllers/UploadController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace WebApp1.Controllers
+{
+    public class UploadController : Controller
+    {
+        // GET: Upload
+        public ActionResult Index()
+        {
+            return View("Index");
+        }
+
+        // Post: Upolad
+        //public ActionResult Create()
+        //{
+        //    return View("Hello Upload");
+
+        //}
+    }
+}
